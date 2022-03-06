5a300dc8ef11febf5748548ca145cba0f552b6c0
Dolev Farhi <farhi.dolev@gmail.com>
2022-03-06 00:10:59 -0500

    Update array_batched_circular_queries.py

--- ch05/array_batched_circular_queries.py
+++ ch05/array_batched_circular_queries.py
@@ -2,28 +2,29 @@
 
 import requests
 
-array_elements = 5
-depth = 5
-query = {"query":"query {"}
-loop_count = 0
+ARRAY_LENGTH = 5
+QUERY_REPEAT = 10 
 
-for _ in range(depth):
-    loop_count += 1
-    close_brace = '} ' * depth * 2  + '}'
+query = {"query":"query {"}
+count = 0
+for _ in range(QUERY_REPEAT):
+    count += 1
+    closing_braces = '} ' * QUERY_REPEAT * 2  + '}'
     payload = "pastes { owner { "
     query["query"] += payload
 
-    if loop_count == depth:
-      query["query"] += 'name' + close_brace
+    if count == QUERY_REPEAT:
+      query["query"] += 'name' + closing_braces
 
-print('===========')
-print(query)
-print('==========')
-print('Depth of Query:', depth * 2 + 1)
+print('Query:', query['query'])
+print('Query Repeated:', QUERY_REPEAT, 'times')
+print('Query Depth:', QUERY_REPEAT * 2 + 1, 'levels')
+print('Array Length:', ARRAY_LENGTH, 'elements')
+
 
 
 queries = []
-for _ in range(array_elements):
+for _ in range(ARRAY_LENGTH):
   queries.append(query)
 
 r = requests.post('http://localhost:5013/graphql', json=queries)
